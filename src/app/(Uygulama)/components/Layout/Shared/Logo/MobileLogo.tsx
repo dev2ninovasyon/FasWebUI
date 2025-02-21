b23c73f88c1254a167ace14f6b4ed78733dee9b5
2NMaster <2NMaster@DESKTOP-KL01N7V>
2025-02-21 10:37:36 +0300

    logo düzenlendi.

--- src/app/(Uygulama)/components/Layout/Shared/Logo/MobileLogo.tsx
+++ src/app/(Uygulama)/components/Layout/Shared/Logo/MobileLogo.tsx
@@ -22,7 +22,7 @@ const MobileLogo = () => {
             alt="logo"
             height={customizer.TopbarHeight}
             width={188}
-            style={{ padding: "8px 4px", height: "auto" }}
+            style={{ padding: "8px 4px", height: "75px" }}
             priority
           />
         ) : (
@@ -31,7 +31,7 @@ const MobileLogo = () => {
             alt="logo"
             height={customizer.TopbarHeight}
             width={188}
-            style={{ padding: "8px 4px", height: "auto" }}
+            style={{ padding: "8px 4px", height: "75px" }}
             priority
           />
         )}
@@ -47,7 +47,7 @@ const MobileLogo = () => {
           alt="logo"
           height={customizer.TopbarHeight}
           width={188}
-          style={{ padding: "8px 8px", height: "auto" }}
+          style={{ padding: "8px 8px", height: "75px" }}
           priority
         />
       ) : (
@@ -56,7 +56,7 @@ const MobileLogo = () => {
           alt="logo"
           height={customizer.TopbarHeight}
           width={188}
-          style={{ padding: "8px 8px", height: "auto" }}
+          style={{ padding: "8px 8px", height: "75px" }}
           priority
         />
       )}
