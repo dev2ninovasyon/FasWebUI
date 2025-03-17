2148e6bc825dd3c59e6936ac3796c880a483f12c
2NMaster <2NMaster@DESKTOP-KL01N7V>
2025-03-17 11:01:38 +0300

    .

--- src/app/(Uygulama)/components/Layout/Vertical/Header/data.ts
+++ src/app/(Uygulama)/components/Layout/Vertical/Header/data.ts
@@ -86,5 +86,104 @@ const profile: ProfileType[] = [
     personal: true,
   },
 ];
+// apps dropdown
 
-export { notifications, profile };
+interface appsLinkType {
+  href: string;
+  title: string;
+  subtext: string;
+  avatar: string;
+}
+
+const appsLink: appsLinkType[] = [
+  {
+    href: "/apps/chats",
+    title: "Chat Application",
+    subtext: "New messages arrived",
+    avatar: "/images/svgs/icon-dd-chat.svg",
+  },
+  {
+    href: "/apps/ecommerce/shop",
+    title: "eCommerce App",
+    subtext: "New stock available",
+    avatar: "/images/svgs/icon-dd-cart.svg",
+  },
+  {
+    href: "/apps/notes",
+    title: "Notes App",
+    subtext: "To-do and Daily tasks",
+    avatar: "/images/svgs/icon-dd-invoice.svg",
+  },
+  {
+    href: "/apps/calendar",
+    title: "Calendar App",
+    subtext: "Get dates",
+    avatar: "/images/svgs/icon-dd-date.svg",
+  },
+  {
+    href: "/apps/contacts",
+    title: "Contact Application",
+    subtext: "2 Unsaved Contacts",
+    avatar: "/images/svgs/icon-dd-mobile.svg",
+  },
+  {
+    href: "/apps/tickets",
+    title: "Tickets App",
+    subtext: "Submit tickets",
+    avatar: "/images/svgs/icon-dd-lifebuoy.svg",
+  },
+  {
+    href: "/apps/email",
+    title: "Email App",
+    subtext: "Get new emails",
+    avatar: "/images/svgs/icon-dd-message-box.svg",
+  },
+  {
+    href: "/apps/blog/post",
+    title: "Blog App",
+    subtext: "added new blog",
+    avatar: "/images/svgs/icon-dd-application.svg",
+  },
+];
+
+interface LinkType {
+  href: string;
+  title: string;
+}
+
+const pageLinks: LinkType[] = [
+  {
+    href: "/theme-pages/pricing",
+    title: "Pricing Page",
+  },
+  {
+    href: "/auth/auth1/login",
+    title: "Authentication Design",
+  },
+  {
+    href: "/auth/auth1/register",
+    title: "Register Now",
+  },
+  {
+    href: "/404",
+    title: "404 Error Page",
+  },
+  {
+    href: "/apps/note",
+    title: "Notes App",
+  },
+  {
+    href: "/apps/user-profile/profile",
+    title: "User Application",
+  },
+  {
+    href: "/apps/blog/post",
+    title: "Blog Design",
+  },
+  {
+    href: "/apps/ecommerce/checkout",
+    title: "Shopping Cart",
+  },
+];
+
+export { notifications, profile, pageLinks, appsLink };
