f72450f94ac5b4083471acc4c4b3081be58e0c0d
2NMaster <2NMaster@DESKTOP-KL01N7V>
2025-08-19 17:59:52 +0300

    .

--- src/app/(Uygulama)/DenetimKanitlari/HileVeUsulsuzluk/HileVeUsulsuzlukLayout.tsx
+++ src/app/(Uygulama)/DenetimKanitlari/HileVeUsulsuzluk/HileVeUsulsuzlukLayout.tsx
@@ -4,7 +4,11 @@ import Breadcrumb from "@/app/(Uygulama)/components/Layout/Shared/Breadcrumb/Bre
 
 const BCrumb = [
   {
-    to: "/HileVeUsulsuzluk",
+    to: "/DenetimKanitlari",
+    title: "Denetim Kanıtları",
+  },
+  {
+    to: "/DenetimKanitlari/HileVeUsulsuzluk",
     title: "Hile Ve Usulsüzlük",
   },
 ];
