ab59d15d300defa1760f5842b7c93e693524f2d3
2NMaster <2NMaster@DESKTOP-KL01N7V>
2025-09-04 14:03:38 +0300

    .

--- src/app/(Uygulama)/GenelKurul/DenetimCalismasiIzleme/page.tsx
+++ src/app/(Uygulama)/GenelKurul/DenetimCalismasiIzleme/page.tsx
@@ -218,53 +218,6 @@ const Page = () => {
             setToplam={setToplam}
           />
         </Box>
-        <Box>
-          {(user.rol?.includes("KaliteKontrolSorumluDenetci") ||
-            user.rol?.includes("SorumluDenetci") ||
-            user.rol?.includes("Denetci") ||
-            user.rol?.includes("DenetciYardimcisi")) && (
-            <Grid
-              container
-              sx={{
-                width: "95%",
-                margin: "0 auto",
-                justifyContent: "space-between",
-              }}
-            >
-              <Grid item xs={12} md={3.9} lg={3.9} mt={3}>
-                <BelgeKontrolCard
-                  hazirlayan="Denetçi - Yardımcı Denetçi"
-                  controller={controller}
-                ></BelgeKontrolCard>
-              </Grid>
-              <Grid item xs={12} md={3.9} lg={3.9} mt={3}>
-                <BelgeKontrolCard
-                  onaylayan="Sorumlu Denetçi"
-                  controller={controller}
-                ></BelgeKontrolCard>
-              </Grid>
-              <Grid item xs={12} md={3.9} lg={3.9} mt={3}>
-                <BelgeKontrolCard
-                  kaliteKontrol="Kalite Kontrol Sorumlu Denetçi"
-                  controller={controller}
-                ></BelgeKontrolCard>
-              </Grid>
-            </Grid>
-          )}
-          <Grid
-            container
-            sx={{
-              width: "95%",
-              margin: "0 auto",
-              justifyContent: "space-between",
-              gap: 1,
-            }}
-          >
-            <Grid item xs={12} lg={12} mt={5}>
-              <IslemlerCard controller={controller} />
-            </Grid>
-          </Grid>
-        </Box>
       </PageContainer>
     </>
   );
