2dd9386207216e5cac8d3ad10e9127c1af8379fd
dev <dev@2ninovasyon.com>
2025-11-21 10:12:43 +0300

    1-

--- src/app/(Uygulama)/Surdurulebilirlik/page.tsx
+++ src/app/(Uygulama)/Surdurulebilirlik/page.tsx
@@ -1,19 +1,52 @@
 "use client";
 
 import PageContainer from "@/app/(Uygulama)/components/Container/PageContainer";
-import { Box } from "@mui/material";
+import KysLayout from "./SurdurulebilirlikLayout";
+import React, { useState } from "react";
+import { Box, Button, IconButton } from "@mui/material";
+import FilteredMenu from "@/app/(Uygulama)/components/Tables/MenuTable";
 import TopCards from "@/app/(Uygulama)/components/Cards/TopCards";
-import SurdurulebilirlikLayout from "./SurdurulebilirlikLayout";
+import { IconLayoutGrid, IconList } from "@tabler/icons-react";
 
 const Page = () => {
+  const [showFilteredMenu, setShowFilteredMenu] = useState(true);
+
+  const handleToggle = () => {
+    setShowFilteredMenu((prev) => !prev);
+  };
   return (
-    <SurdurulebilirlikLayout>
-      <PageContainer title="SÜRDÜRÜLEBİLİRLİK" description="this is Sürdürülebilirlik">
+    <KysLayout>
+      <PageContainer
+        title="SÜRDÜRÜLEBİLİRLİK"
+        description="this is SÜRDÜRÜLEBİLİRLİK"
+      >
         <Box>
-          <TopCards title="SÜRDÜRÜLEBİLİRLİK" />
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "end",
+              paddingBottom: showFilteredMenu ? "32px" : "0px",
+              paddingRight: "10px",
+            }}
+          >
+            <Button onClick={handleToggle}>
+              {showFilteredMenu ? (
+                <IconLayoutGrid size={24} />
+              ) : (
+                <IconList size={24} />
+              )}
+              {/* Toggle between icons */}
+            </Button>
+          </Box>
+
+          {showFilteredMenu ? (
+            <FilteredMenu title="SÜRDÜRÜLEBİLİRLİK" />
+          ) : (
+            <TopCards title="SÜRDÜRÜLEBİLİRLİK" />
+          )}
         </Box>
       </PageContainer>
-    </SurdurulebilirlikLayout>
+    </KysLayout>
   );
 };
 
