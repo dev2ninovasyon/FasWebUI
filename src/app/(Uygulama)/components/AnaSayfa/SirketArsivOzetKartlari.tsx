d8403b7d533c4f0d0887cb15f71f6c687647c91a
Your Name <melisakpl@hotmail.com>
2025-12-02 17:08:03 +0300

    anasayfa ve giriş sayfası tasarımları yapıldı.

--- src/app/(Uygulama)/components/AnaSayfa/SirketArsivOzetKartlari.tsx
+++ src/app/(Uygulama)/components/AnaSayfa/SirketArsivOzetKartlari.tsx
@@ -15,7 +15,11 @@ import {
   TableCell,
   TableHead,
   TableRow,
+  Link,
 } from "@mui/material";
+import NextLink from "next/link";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import TouchAppIcon from "@mui/icons-material/TouchApp";
 import Image from "next/image";
 import { useTheme } from "@mui/material/styles";
 import { useSelector } from "@/store/hooks";
@@ -25,7 +29,9 @@ const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 import {
   getSirketArsivOzet,
   SirketArsivOzetDto,
+  UserActionDto,
 } from "@/api/AnaSayfa/AnaSayfa";
+import { orange } from "@mui/material/colors";
 
 export function SirketArsivOzetKartlari() {
   const theme = useTheme();
@@ -34,6 +40,7 @@ export function SirketArsivOzetKartlari() {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState<SirketArsivOzetDto | null>(null);
   const [error, setError] = useState<string | null>(null);
+  const [actions, setActions] = useState<UserActionDto[]>([]);
 
   useEffect(() => {
     if (!user.token || !user.id) {
@@ -126,53 +133,295 @@ export function SirketArsivOzetKartlari() {
       },
     },
   };
-
+const getCardPalette = (index: number) => {
+    const palettes = [
+      { bg: theme.palette.primary.light, text: theme.palette.primary.main },
+      { bg: theme.palette.warning.light, text: theme.palette.warning.main },
+      { bg: theme.palette.info.light, text: theme.palette.info.main },
+      { bg: theme.palette.error.light, text: theme.palette.error.main },
+      { bg: theme.palette.success.light, text: theme.palette.success.main },
+      { bg: theme.palette.secondary.light, text: theme.palette.secondary.main },
+    ];
+    return palettes[index % palettes.length];
+  };
   const barSeries = [
     {
       name: "Arşiv Alanı (MB)",
       data: donutSeries,
     },
   ];
+  
   const total = Array.isArray(donutSeries)
     ? donutSeries.reduce((sum, val) => sum + val, 0)
     : donutSeries;
+
+
+  const toplamDosyaSayisi = data.sirketler.reduce(
+    (sum, s) => sum + s.dosyaSayisi,
+    0
+  );
+
+
+      
   return (
     <Box mt={4}>
       <Typography variant="h6" gutterBottom>
+        Şirketler ve Modüller
+      </Typography>
+      <Typography variant="body2" color="textSecondary" sx={{ mb: 2 }} >
+        Kullanıcıya tanımlı şirketlerin ve modüllerin sayısı.
+      </Typography>
+
+      <Grid container spacing={3}columns={{ xs: 1, sm: 2, md: 4 }}>
+          <Grid item xs={12} sm={6} md={1}>
+          <Card
+            sx={{
+              borderWidth: 3,
+              borderStyle: "solid",
+              borderColor: "primary.main",
+              width: "100%",
+              height: "100%",
+              borderRadius: 3,
+              boxShadow: "0 4px 16px rgba(0,0,0,0.06)",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              textAlign: "center",
+              p: 2,
+            }}
+          >
+            <CardContent>
+              <Image
+                src="/images/svgs/icon-company.svg"
+                alt="Şirket"
+                width={65}
+                height={65}
+              />
+              <Typography
+                color="primary.main"
+                mt={1}
+                variant="subtitle1"
+                fontWeight={600}
+              >
+                Şirket Sayısı
+              </Typography>
+              <Typography color="primary.main" variant="h4" fontWeight={600}>
+                <Link
+                  underline="hover" 
+                  sx={{ 
+                    display: "flex", 
+                    alignItems: "center", 
+                    gap: 1,
+                    "&:hover svg": {
+                      transform: "translateX(15px)",
+                      transition: "0.1s",
+                    },
+                  }}
+                >
+                <ChevronRightIcon />
+                {data.toplamSirketSayisi}
+                </Link>
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={1}>
+          <Card
+            sx={{
+              borderWidth: 3,
+              borderStyle: "solid",
+              borderColor: "orange",
+              width: "100%",
+              height: "100%",
+              borderRadius: 3,
+              boxShadow: "0 4px 16px rgba(0,0,0,0.06)",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              textAlign: "center",
+              p: 2,
+            }}
+          >
+            <CardContent>
+              <Image
+                src="/images/svgs/icon-modules.svg"
+                alt="Modül Sayısı"
+                width={65}
+                height={65}
+              />
+              <Typography
+                color= "orange"
+                mt={1}
+                variant="subtitle1"
+                fontWeight={600}
+              >
+                Modül Sayısı
+              </Typography>
+              <Typography color="orange" variant="h4" fontWeight={600}>
+                <Link
+                  color={orange}
+                  underline="hover" 
+                  sx={{ 
+                    display: "flex", 
+                    alignItems: "center", 
+                    gap: 1,
+                    "&:hover svg": {
+                      transform: "translateX(15px)",
+                      transition: "0.1s",
+                    },
+                  }}
+                >
+                <ChevronRightIcon />
+                  8
+                </Link>
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+
+      
+        <Grid item xs={12} sm={6} md={1}>
+          <Card
+            sx={{
+              borderWidth: 3,
+              borderStyle: "solid",
+              borderColor: "green",
+              width: "100%",
+              height: "100%",
+              borderRadius: 3,
+              boxShadow: "0 4px 16px rgba(0,0,0,0.06)",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              textAlign: "center",
+              p: 2,
+            }}
+          >
+            <CardContent>
+              <Image
+                src="/images/svgs/icon-active-modules.svg"
+                alt="Aktif Modüller"
+                width={65}
+                height={65}
+              />
+              <Typography
+                color="green"
+                mt={1}
+                variant="subtitle1"
+                fontWeight={600}
+              >
+                Aktif Modüller
+              </Typography>
+              <Typography color="green" variant="h4" fontWeight={600}>
+                <Link
+                  
+                  underline="hover" 
+                  sx={{ 
+                    display: "flex", 
+                    alignItems: "center",
+                    color: "green", 
+                    gap: 1,
+                    "&:hover svg": {
+                      transform: "translateX(15px)",
+                      transition: "0.1s",
+                    },
+                  }}
+                >
+                <ChevronRightIcon />
+                {toplamDosyaSayisi}
+                </Link>
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        
+        <Grid item xs={12} sm={6} md={1}>
+          <Card
+            sx={{
+              borderWidth: 3,
+              borderStyle: "solid",
+              borderColor: "#ba68c8",
+              width: "100%",
+              height: "100%",
+              borderRadius: 3,
+              boxShadow: "0 4px 16px rgba(0,0,0,0.06)",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              textAlign: "center",
+              p: 2,
+              color: "#ba68c8",
+            }}
+          >
+            <CardContent>
+              <Image
+                src="/images/svgs/icon-more-modules.svg"
+                alt="Daha Fazla Modül İçin.."
+                width={65}
+                height={65}
+              />
+              <Typography
+                mt={1}
+                variant="subtitle1"
+                fontWeight={600}
+                sx={{color:"#ba68c8"}}
+              > 
+              <Link
+                underline="hover"
+                sx={{
+                  display: "inline-flex",   // Yan yana hizala
+                  alignItems: "center",     // Dikey ortala
+                  gap: 0.5,                   // Yazı ile ikon arası mesafe
+                  color:"#ba68c8",
+                }}
+                
+              >
+                Daha Fazla Modül
+                <TouchAppIcon
+                  sx={{
+                    transform: "rotate(180deg)", //Parmağı aşağı çeviriyoruz
+                    fontSize: "1.5rem",
+                    color:"#ba68c8",
+                  }}/>
+              </Link>
+              </Typography>
+              <Typography  variant="h6" fontWeight={600}>
+                <Link 
+                  component={NextLink}
+                  href = "https://fasmart.app/denetim-araclari"  
+                  underline="hover" 
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  sx={{ 
+                    display: "flex", 
+                    alignItems: "center", 
+                    gap: 1,
+                    color:"#ba68c8",
+                    "&:hover svg": {
+                      transform: "translateX(15px)",
+                      transition: "0.1s",
+                    },
+                  }}
+                >
+                  <ChevronRightIcon />
+                  FAS Modülleri
+                </Link>
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+
+<br></br>
+<Typography variant="h6" gutterBottom>
         Şirketler ve Arşiv Alanı Kullanımı
       </Typography>
       <Typography variant="body2" color="textSecondary" sx={{ mb: 2 }}>
         Kullanıcıya tanımlı şirketlerin sayısı ve bu şirketlerin arşivde
         kapladığı alanlar.
       </Typography>
-
-      <Grid container spacing={3}>
-
-        <Grid item xs={12} sm={4} lg={2}>
-          <Box bgcolor={"primary.light"} textAlign="center">
-            <CardContent>
-              <Image src="images/svgs/icon-briefcase.svg" alt={"topcard.icon"} width="50" height="50" />
-              <Typography
-                color={"primary.light" + ".main"}
-                mt={1}
-                variant="subtitle1"
-                fontWeight={600}
-              >
-                Şirket Sayisi
-              </Typography>
-              <Typography
-                color={"primary.light" + ".main"}
-                variant="h4"
-                fontWeight={600}
-              >
-                {data.toplamSirketSayisi}
-              </Typography>
-            </CardContent>
-          </Box>
-        </Grid>
-
-      </Grid>
-
 
       {/* Bar chart */}
       <Grid item xs={12} md={6}>
