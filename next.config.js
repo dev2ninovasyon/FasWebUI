efdb56a031270a34d3385fe4b5435e6d9edaabeb
Your Name <melisakpl@hotmail.com>
2026-03-02 14:50:33 +0300

    .

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
   },
   reactStrictMode: false,
   output: "standalone",
+  turbopack: {
+    root: "c:/Users/lenov/source/repos/dev2ninovasyon/FasWebUI",
+  },
   typescript: {
     ignoreBuildErrors: false,
   },
