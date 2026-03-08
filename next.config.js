cd94884a6f4db9a53d150b4126c5e3cb952ca4b3
lenov <dev@2ninovasyon.com>
2026-03-08 04:57:18 +0300

    feat: Karşılaştırmalı Analiz verilerini PowerBI için dışa aktarma özelliği eklendi ve next.config.js'deki modüler içe aktarma ayarları yorum sat

--- next.config.js
+++ next.config.js
@@ -1,18 +1,22 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
+  /*
   modularizeImports: {
     "@mui/icons-material": {
       transform: "@mui/icons-material/{{member}}",
     },
   },
+  */
   reactStrictMode: false,
   output: "standalone",
   typescript: {
     ignoreBuildErrors: false,
   },
+  /*
   experimental: {
     optimizePackageImports: ['@mui/material', '@mui/icons-material', '@tabler/icons-react', 'lucide-react'],
   },
+  */
 };
 
 module.exports = nextConfig;
